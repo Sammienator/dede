15dd545d40a4df3c08a2cd0efc058bf9116aca4a
Sammienator <sammugo18@gmail.com>
2021-11-09 20:00:40 +0300

    changes to scrolling

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;}
+
+
+
+
+
+
+
 // navigation bar 
 const navShowBtn = document.querySelector('.nav-show-btn');
 const navHideBtn = document.querySelector('.nav-hide-btn');
